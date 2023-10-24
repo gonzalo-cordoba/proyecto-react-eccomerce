d2f333f191c240f0bf5513b0c6998a21d9592753
Gonzalo Cordoba <GonzaloCordoba070@gmail.com>
2023-10-24 16:41:14 -0300

    creacion de navbar

---
+++ appvite/src/components/CartWidget.jsx
@@ -0,0 +1,12 @@
+import carrito from '../assets/react.svg'
+
+export const CartWidget = () => {
+
+    return (
+        <a href="#" className='nav__car'>
+            <img src="https://svgsilh.com/svg/306793.svg" width="30" height="25" alt="Cart" />
+            <p>0</p>
+        </a>
+        )
+
+}
