fffb420e0cc47160a5f2f1249b8a0a15fbe46e76
Gonzalo Cordoba <GonzaloCordoba070@gmail.com>
2023-10-26 13:13:21 -0300

    Cambio de imagen de cart

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,10 +1,10 @@
-import carrito from '../assets/react.svg'
+import cart from '../assets/cart.png'
 
 export const CartWidget = () => {
 
     return (
         <a href="#" className='nav__car'>
-            <img src="https://svgsilh.com/svg/306793.svg" width="30" height="25" alt="Cart" />
+            <img src={cart} width="30" height="25" alt="Cart" />
             <p>0</p>
         </a>
         )
